8fcd0af4a7c2bff2a44c8180d43e56e8b30102ef
Scot P. Floess <flossware@gmail.com>
2017-11-23 09:18:31 -0500

    Crontab was wrong

--- bin/backups.sh
+++ bin/backups.sh
@@ -2,12 +2,12 @@
 
 echo "Start time:  `date`"
 
-echo "   Running rsync..."
+echo "   Running rsync...param(s):  [$*]"
 
-echo "       Backing up laptop    - using param(s) [$*]"
+echo "       Backing up laptop"
 rsync -av root@laptop:/home/sfloess /opt/nas/backups/host/redhat-laptop/home $* --exclude .m2/repository --exclude Music --exclude Videos --exclude Downloads --exclude redhatCache 1>/dev/null 2>/dev/null
 
-echo "       Backing up to host-3 - using param(s) [$*]"
+echo "       Backing up to host-3"
 rsync -av /opt/nas/backups/host root@host-3:/backups $* 1>/dev/null 2>/dev/null
 
 echo "End time:    `date`"
